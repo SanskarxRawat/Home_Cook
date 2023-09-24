0be2c4c68e7d3f4696ab3c0f618f66ed6fcd2a45
Sanskar Rawat <rawatavi.211@gmail.com>
2023-09-24 13:03:15 +0530

    "path removed"

--- main.js
+++ main.js
@@ -29,8 +29,6 @@ mongoose.Promise = global.Promise;
 
 app.set('port', process.env.PORT || 3000);
 
-app.set('views', path.join(__dirname, 'views'));
-
 app.set('view engine', 'ejs');
 
 app.set('token', process.env.TOKEN || "recipeT0K3N");
